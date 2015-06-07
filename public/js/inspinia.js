6eb63a8ddaf688965fa065e85deca4ff74f3ceaf
xyclr <xyclr@163.com>
2015-06-07 22:53:38 +0800

    xx

--- public/js/inspinia.js
+++ public/js/inspinia.js
@@ -7,6 +7,23 @@
 
 $(document).ready(function () {
 
+    //toastr global setting
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "400",
+        "hideDuration": "1000",
+        "timeOut": "7000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
     // Add body-small class if window less than 768px
     if ($(this).width() < 769) {
         $('body').addClass('body-small')
