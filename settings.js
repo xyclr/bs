d2d1f9ae3e22b39d8f7400d05bbd239852c9aa43
xyclr <xyclr@163.com>
2015-08-09 10:46:28 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 module.exports = { 
   cookieSecret: 'bs',
   db: 'bs',
-  host: 'localhost',
+  host: '45.79.9.136',
+  port: '27017',
   staticSever : './public',
   uploadPath : "./public/uploads",
   stServer : "http://st.ndesig.com",
   Server : "http://www.ndesig.com"
 }; 
-
